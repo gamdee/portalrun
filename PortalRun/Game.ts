30cbe80f2970b5deb2e943f6e63f41af40fe89ef
Luong Hoang <lhoang@live.com>
2014-08-08 00:23:03 -0400

    resized game to full window size

--- PortalRun/Game.ts
+++ PortalRun/Game.ts
@@ -1,9 +1,10 @@
-﻿module PortalRun {
+﻿/// <reference path="jquery.d.ts" />
+module PortalRun {
 
     export class Game extends Phaser.Game {
         constructor() {
 
-            super(800, 400, Phaser.AUTO, null, null);
+            super($(window).width(), $(window).height(), Phaser.AUTO, null, null);
 
             this.state.add('Boot', Boot, false);
             this.state.add('Preloader', Preloader, false);
