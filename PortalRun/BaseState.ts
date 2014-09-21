938a3511ea57bef7edeaa277e2b57fb5a59e01f6
Luong Hoang <lhoang@live.com>
2014-09-21 10:45:17 -0400

    fixed resize

--- PortalRun/BaseState.ts
+++ PortalRun/BaseState.ts
@@ -3,21 +3,11 @@
     export class BaseState extends Phaser.State {
 
         resizeScreen() {
-            if (this.game.device.desktop) {
-                //  If you have any desktop specific settings, they can go in here
-                this.game.scale.pageAlignHorizontally = true;
-                this.game.scale.pageAlignVertically = true;
-                this.game.scale.setShowAll();
-                this.game.scale.setScreenSize(true);
-            }
-            else {
                 //  Same goes for mobile settings.
                 this.game.scale.pageAlignHorizontally = true;
                 this.game.scale.pageAlignVertically = true;
                 this.game.scale.setShowAll();
                 this.game.scale.setScreenSize(true);
-                this.game.scale.forceOrientation(true, false, 'rotate');
-            }
         }
 
         update() {
